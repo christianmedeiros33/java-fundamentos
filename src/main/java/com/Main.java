f0496d732732c93500b4316ff014925c644753e0
Your Name <christianmedeiros022@gmail.com>
2023-09-21 23:03:13 -0300

    usando o While, Do While e for.

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,4 +1,4 @@
-package br.com;
+package com;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/com/javaFundamentos/logica/While_Do_While_For.java
@@ -0,0 +1,33 @@
+package com.javaFundamentos.logica;
+
+public class While_Do_While_For {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 10) {
+            System.out.println(count);
+            count++;
+        }
+
+        System.out.println("----");
+
+        count = 0;
+        do {
+            System.out.println("Dentro do dowhile " + ++count);
+        } while (count < 10);
+
+        System.out.println("----");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("For para " + i);
+        }
+
+        System.out.println("----");
+        // exercise number is pair
+        for (int i = 0; i < 1000; i++) {
+            if (i % 2 == 0){
+                System.out.println("number is pair: " + i);
+            }
+        }
+    }
+}
