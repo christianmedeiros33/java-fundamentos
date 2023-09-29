dcd361de3541822b083a7ede0eccb94c42a29c56
Your Name <christianmedeiros022@gmail.com>
2023-09-28 23:48:16 -0300

    bloco de inicialização static, executa somente uma vez quando chamado.

---
+++ src/main/java/com/javaFundamentos/javacore/modifierStatic/initializationBlocksStatic/model/Anime.java
@@ -0,0 +1,55 @@
+package com.javaFundamentos.javacore.modifierStatic.initializationBlocksStatic.model;
+
+public class Anime {
+    private String name;
+    private static int[] episodes;
+
+    // 0- bloco de inicialização e executado quando a JVM carregar class
+    // 1- alocação espaço em memória para o objeto
+    // 2- cada atributo de classe criado e inicializado com valores default ou o quer for passado
+    // 3- Bloco de inicialização e executado
+    // 4- Construtor e executado
+
+    static {
+        System.out.println("dentro do inicialização");
+        episodes = new int[10];
+        for (int i = 0; i < episodes.length; i++) {
+            episodes[i] = i + 1;
+        }
+    }
+
+    static {
+        System.out.println("dentro do inicialização 2");
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public Anime() {
+
+        for (int episodes : Anime.episodes
+        ) {
+            System.out.print(episodes + " ");
+        }
+        System.out.println();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[] getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int[] episodes) {
+        this.episodes = episodes;
+    }
+
+
+}
---
+++ src/main/java/com/javaFundamentos/javacore/modifierStatic/initializationBlocksStatic/test/AnimeTest.java
@@ -0,0 +1,11 @@
+package com.javaFundamentos.javacore.modifierStatic.initializationBlocksStatic.test;
+
+import com.javaFundamentos.javacore.modifierStatic.initializationBlocksStatic.model.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        Anime animeTwo = new Anime();
+        Anime animeThree = new Anime();
+    }
+}
